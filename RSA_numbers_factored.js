7e2fd29a5791b51bd7545a822354aba1efb77fd2
Hermann-SW <hermann@stamm-wilbrandt.de>
2022-12-26 08:35:14 +0000

    Take itertools.combinations Javascript implementations with link

--- RSA_numbers_factored.js
+++ RSA_numbers_factored.js
@@ -174,6 +174,26 @@ function gcd(a, b){
 function lcm(a, b){
     return a * b / gcd(a, b);
 } 
+
+// from itertools import combinations, combinations_with_replacement, chain
+//
+// from https://stackoverflow.com/a/54385026/5674289
+//
+function* range(start, end) {
+  for (; start <= end; ++start) { yield start; }
+}
+function last(arr) { return arr[arr.length - 1]; }
+function* numericCombinations(n, r, loc = []) {
+  const idx = loc.length;
+  if (idx === r) {
+    yield loc;
+    return;
+  }
+  for (let next of range(idx ? last(loc) + 1 : 0, n - r + idx)) { yield* numericCombinations(n, r, loc.concat(next)); }
+}
+function* combinations(arr, r) {
+  for (let idxs of numericCombinations(arr.length, r)) { yield idxs.map(i => arr[i]); }
+}
 //
 ///////////////////////////////////
 
