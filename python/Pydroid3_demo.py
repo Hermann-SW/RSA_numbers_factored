b7be0e7e3b1459d843c2484cb78c79cf9c76bd64
Hermann-SW <hermann@stamm-wilbrandt.de>
2023-05-01 15:03:05 +0200

    Pydroid3 demo: just RSA().validate()

--- python/Pydroid3_demo.py
+++ python/Pydroid3_demo.py
@@ -1,11 +1,3 @@
 from RSA_numbers_factored import *
 
-R = RSA()
-r = R.factored_2()[-1]
-l, n, p, q, pm1, qm1 = r
-assert (p - 1) * (q - 1) == R.totient(r)
-assert R.totient_2(r) == R.totient_2(l)
-assert R.totient_2(r) == dictprod_totient(pm1, qm1)
-assert pow(65537, R.reduced_totient_2(190), R.reduced_totient(190)) == 1
-
-R.validate()
+RSA().validate()
