b1264c21f91300f5722f6dc6b4421cdfe0f987d0
Hermann-SW <hermann@stamm-wilbrandt.de>
2023-05-11 19:36:31 +0200

    make creation of "mpz" more clear

--- python/sq2.py
+++ python/sq2.py
@@ -7,9 +7,8 @@ from sys import stderr, argv
 from RSA_numbers_factored import sq2
 
 if argv[-1] == "gmpy2":
-    from gmpy2 import mpz  # pylint: disable=no-name-in-module
-else:
-    mpz = lambda x: x
+    from gmpy2 import mpz                  # pylint: disable=no-name-in-module
+else:                 mpz = lambda x: x
 
 start = time()
 t = sq2(mpz(2 ** 2 ** 13 + 897))
