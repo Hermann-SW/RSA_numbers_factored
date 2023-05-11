5ff51afad2588ca8c41ff74202582d326f982bb9
Hermann-SW <hermann@stamm-wilbrandt.de>
2023-05-11 11:19:25 +0200

    Make sq2.py pylint clean

--- python/sq2.py
+++ python/sq2.py
@@ -1,13 +1,15 @@
+# pylint: disable=C0103
+#                 invalid-name
 """determine sum of squares for 2466-digit prime"""
 from time import time
 from sys import stderr, argv
-if len(argv) > 1 and argv[1] == "gmpy2":
-    from gmpy2 import mpz
-else:
-    def mpz(x):
-        return x
 
 from RSA_numbers_factored import sq2
+
+if len(argv) > 1 and argv[1] == "gmpy2":
+    from gmpy2 import mpz  # pylint: disable=no-name-in-module
+else:
+    mpz = lambda x: x
 
 start = time()
 t = sq2(mpz(2 ** 2 ** 13 + 897))
